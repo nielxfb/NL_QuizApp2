f6ae6fba83118f448c9ca946866c9b10c06b5261
Daniel Adamlu <adamludnl@gmail.com>
2024-10-23 16:46:29 +0700

    feat: default value for dates

--- QuizApp.Application/DTOs/Schedule/AddScheduleDto.cs
+++ QuizApp.Application/DTOs/Schedule/AddScheduleDto.cs
@@ -3,6 +3,6 @@ namespace QuizApp.Application.DTOs.Schedule;
 public class AddScheduleDto
 {
     public Guid QuizId { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateTime StartDate { get; set; } = DateTime.Today;
+    public DateTime EndDate { get; set; } = DateTime.Today;
 }
