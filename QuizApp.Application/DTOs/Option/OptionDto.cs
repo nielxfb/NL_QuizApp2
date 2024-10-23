deb05fcc70b47f21e152aab08c399e9a2164c23a
Daniel Adamlu <adamludnl@gmail.com>
2024-10-23 16:46:14 +0700

    feat: default value for option

--- QuizApp.Application/DTOs/Option/OptionDto.cs
+++ QuizApp.Application/DTOs/Option/OptionDto.cs
@@ -3,7 +3,7 @@ namespace QuizApp.Application.DTOs.Option;
 public class OptionDto
 {
     public Guid QuestionId { get; set; }
-    public char OptionChoice { get; set; }
+    public char OptionChoice { get; set; } = 'A';
     public string OptionText { get; set; }
     public string? ImageUrl { get; set; }
     public bool IsCorrect { get; set; }
